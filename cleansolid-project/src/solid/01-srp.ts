0e9178cd45f33e71952b3ac3d69571c0e685a876
zmanangon09 <a.manangon09@gmail.com>
2026-06-10 07:59:34 -0500

    refactor: extraer logica de suscripciones a nueva clase SubscriptionBloc

--- cleansolid-project/src/solid/01-srp.ts
+++ cleansolid-project/src/solid/01-srp.ts
@@ -21,16 +21,19 @@ class UserBloc {
         console.log('Enviando correo a los usuarios');
     }
 
+
+}
+class SubscriptionBloc {
     onAddSubscription( subscriptionId: number ) {
-        // Simula la gestión de suscripciones
         console.log('Agregando suscripción:', subscriptionId );
     }
 
 }
 
+const subscriptionBloc = new SubscriptionBloc();
 const userBloc = new UserBloc();
 
 userBloc.loadUser(10);
 userBloc.saveUser({ id: 10, name: 'Fernando' });
 userBloc.notifyUser();
-userBloc.onAddSubscription(1234);
+subscriptionBloc.onAddSubscription(1234);
